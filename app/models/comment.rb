6edba343e6091da18994c79f0fd1f409b5069f4f
Eric Draut <edraut@gmail.com>
2011-07-18 13:18:37 -0600

    connected heads up comment alerts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,7 +11,7 @@ class Comment < ActiveRecord::Base
   def handle_alerts
     alertees = self.commentable.heads_ups.where("heads_ups.user_id != #{self.user_id}")
     alertees.each do |alertee|
-      Notifier.comment_alert(alertee,self).deliver
+      Notifier.comment_alert(alertee.user,self).deliver
     end
   end
 end
--- app/models/project_sharer.rb
+++ app/models/project_sharer.rb
@@ -2,11 +2,18 @@ class ProjectSharer < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   belongs_to :folder
+  after_create :handle_heads_ups
   after_destroy :transfer_tasks_to_owner
   
   scope :active, joins(:project).where("projects.state = 'active'")
 
   validates_uniqueness_of :user_id, :scope => :project_id
+  def handle_heads_ups
+    heads_up = HeadsUp.where(:user_id => self.user_id,:alertable_id => self.project.id,:alertable_type => 'Project').first
+    heads_up ||= HeadsUp.create(:user_id => self.user_id,:alertable_id => self.project.id,:alertable_type => 'Project')
+    owner_heads_up = HeadsUp.where(:user_id => self.project.user_id,:alertable_id => self.project.id,:alertable_type => 'Project').first
+    owner_heads_up ||= HeadsUp.create(:user_id => self.project.user_id,:alertable_id => self.project.id,:alertable_type => 'Project')
+  end
   
   def transfer_tasks_to_owner
     Task.update_all "user_id = #{self.project.user_id}" , "user_id = #{self.user_id} AND project_id = #{self.project_id}"
