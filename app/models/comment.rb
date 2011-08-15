975e5013cf96ba9b28a7e86322b0c93e0709bfca
Eric Draut <edraut@gmail.com>
2011-08-15 15:51:51 -0600

    added schedule back to mobile dashboard

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   
   scope :by_date, order('updated_at')
-  scope :recent, order('created_at desc')
+  scope :recent, order('comments.created_at desc').where("comments.created_at > (current_date at time zone 'UTC' - interval '2 week')")
   scope :two, limit(2)
   
   after_create :handle_alerts
